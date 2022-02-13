9397cead51e8e08a95fd2bb956cb2002998bbb0a
otinu <fitshter@gmail.com>
2022-02-14 07:47:14 +0900

    [add]Code1_2

--- Code1_1.java
+++ Code1_1.java
@@ -3,6 +3,8 @@ package sukkiri_practice;
 public class Code1_1 {
 	public static void main(String[] args) {
 		Hero hero = new Hero("クリンリ", 300, 20);
-		System.out.println(hero);
+		Hero hero2 = new Hero("クリンリ", 100, 5);
+		System.out.println(hero.equals(hero2));
+		System.out.println(hero == hero2);
 	}
 }
--- Hero.java
+++ Hero.java
@@ -14,4 +14,15 @@ public class Hero {
 	public String toString() {
 		return ("勇者(名前=" + this.name + " HP=" + this.hp + " MP=" + this.mp + ")");
 	}
+
+	public boolean equals(Object o) {
+		if(o == this) return true;	// 自分自身の比較はtrue
+		if(o == null) return false;
+		if(!(o instanceof Hero)) return false;	// Hero型以外の型であればfalse
+		Hero h = (Hero)o;
+		if(!this.name.equals(h.name)) {
+			return false;
+		}
+		return true;
+	}
 }
