49e0dd51d239b6ea23d958a22dc84eb5c309a3a1
otinu <fitshter@gmail.com>
2022-02-15 08:58:01 +0900

    [add]Code1-4

---
+++ Code1_4.java
@@ -0,0 +1,16 @@
+package sukkiri_practice;
+
+import java.util.Arrays;
+
+public class Code1_4 {
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = {1, 2, 3, 4, 5};
+
+		// Arraysのequalsメソッドを使用
+		System.out.println("誤った判定: " + a.equals(b));
+		System.out.println("正しい判定:" + Arrays.equals(a, b));
+	}
+
+}
