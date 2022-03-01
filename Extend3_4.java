79883fef5d899ad9afbcbb6c0e419bf8ec078f48
otinu <fitshter@gmail.com>
2022-03-01 19:47:05 +0900

    [add]Extend3-4

--- Extend3_4.java
+++ Extend3_4.java
@@ -5,9 +5,12 @@ import java.util.function.IntToDoubleFunction;
 public class Extend3_4 {
 
 	public static void main(String[] args) {
+
+		// ラムダ式の外側で定義
 		double b = 3.14;
+
 		IntToDoubleFunction func = (x) -> {
-			return x * x * b;
+			return x * x * b; // ラムダ式の内側で呼び出し ⇒ 変数bをクロージャと呼ぶ
 		};
 
 		System.out.println(func.applyAsDouble(2));
