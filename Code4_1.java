5e62b9a3fd3f60e0825b7fcc4f60a24057ae1b9d
otinu <fitshter@gmail.com>
2022-03-04 07:20:11 +0900

    [add]Code4-1

---
+++ Code4_1.java
@@ -0,0 +1,15 @@
+package sukkiri_practice;
+
+public class Code4_1 {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		int c = a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + b;
+
+		// プログラムを強制終了させることもできる
+		System.exit(1);
+
+	}
+
+}
