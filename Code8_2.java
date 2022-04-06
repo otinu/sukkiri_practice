bbd3beb6578838c1b95a2dc9c232a15da1d53f6a
otinu <fitshter@gmail.com>
2022-04-07 07:22:50 +0900

    [add]Code8-2

---
+++ Code8_2.java
@@ -0,0 +1,28 @@
+package sukkiri_practice;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Code8_2 {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket sock = new Socket("dokojava.jp", 80);
+		InputStream is = sock.getInputStream();
+		OutputStream os = sock.getOutputStream();
+		os.write("GET /index.html HTTP/1.0\r\n".getBytes());
+		os.write("\r\n".getBytes());
+		os.flush();
+		InputStreamReader isr = new InputStreamReader(is);
+		int i = isr.read();
+		while(i != -1) {
+			System.out.print((char) i);
+			i = isr.read();
+		}
+		sock.close();
+	}
+
+}
