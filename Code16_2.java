2cc158f198674da0ada418d3dbd23645b591689f
otinu <fitshter@gmail.com>
2022-04-21 20:01:47 +0900

    [add]Code16-2

---
+++ Code16_2.java
@@ -0,0 +1,17 @@
+package sukkiri_practice;
+
+import java.util.Scanner;
+
+public class Code16_2 {
+
+	public static void main(String[] args) {
+		System.out.println("止めるには「STOP」を入力してください");
+		System.out.println("カウントダウンを開始します");
+		Thread t = new PrintingThread();
+		t.start();
+		String input = new Scanner(System.in).nextLine();
+		System.out.println("入力文字列:" + input);
+		System.out.println("停止処理は未作成です");
+	}
+
+}
---
+++ PrintingThread.java
@@ -0,0 +1,17 @@
+package sukkiri_practice;
+
+import java.util.concurrent.TimeUnit;
+
+public class PrintingThread extends Thread {
+	public void run() {
+		for(int i = 9; i >= 0; i--) {
+			System.out.print(i + "..");
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch(InterruptedException e) {
+				;
+			}
+		}
+	}
+
+}
