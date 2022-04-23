c6d26bc0f0e4fa31b4cfdf04fecbf38e2af613ca
otinu <fitshter@gmail.com>
2022-04-23 19:33:44 +0900

    [add]Code16-3

--- PrintingThread.java
+++ PrintingThread.java
@@ -1,17 +1,19 @@
-package sukkiri_practice;
-
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PrintingThread extends Thread {
-	public void run() {
-		for(int i = 9; i >= 0; i--) {
-			System.out.print(i + "..");
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch(InterruptedException e) {
-				;
-			}
-		}
-	}
+  // スレッド中断の要請を管理するフィールド
+  final AtomicBoolean stopReq = new AtomicBoolean(false);
 
+  public void run() {
+    for (int i = 9; i >= 0; i--) {
+      if (this.stopReq.get()) {
+        break;
+      }
+      System.out.print (i + "..");
+      try {
+        TimeUnit.SECONDS.sleep(1);
+      } catch (InterruptedException e) { ; }
+    }
+  }
 }
