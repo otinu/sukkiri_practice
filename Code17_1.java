bd1c1d71dd7354d149b3e58e7614384eb9405992
otinu <fitshter@gmail.com>
2022-04-24 20:43:25 +0900

    [add]Code17-1

---
+++ Code17_1.java
@@ -0,0 +1,15 @@
+package sukkiri_practice;
+
+import javax.swing.JFrame;
+
+public class Code17_1 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("はじめてのSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 200);
+		frame.setVisible(true);
+		System.out.println("フレームを表示");
+	}
+
+}
